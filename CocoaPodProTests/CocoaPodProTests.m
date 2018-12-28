9fd61ea0af8eb10e174967b4bc50b6c3238fd660
lihui <lihui@eastmoney.com>
2018-12-28 10:34:09 +0800

    Initial Commit

---
+++ CocoaPodProTests/CocoaPodProTests.m
@@ -0,0 +1,37 @@
+//
+//  CocoaPodProTests.m
+//  CocoaPodProTests
+//
+//  Created by Lee on 2018/12/28.
+//  Copyright © 2018 Lee. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CocoaPodProTests : XCTestCase
+
+@end
+
+@implementation CocoaPodProTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
